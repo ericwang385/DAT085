167b118eea33fdf471e29ee7762f6dfa2398bf5b
ericwang385 <eric.w385@hotmail.com>
2021-12-03 13:28:14 +0100

    Add Purpose def

--- PIO/src/Purpose.hs
+++ PIO/src/Purpose.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 module Purpose where
 
-data Ord p => Purpose p = 
-    
+
+
+
+data Purpose = All | Nil | Register | Ads
+
+class Flows (l1 :: Purpose) (l2 :: Purpose)
+instance Flows All All 
+instance Flows Nil Nil 
+instance Flows Register Register
+instance Flows Ads Ads 
+instance Flows Nil Register
+instance Flows Nil Ads
+instance Flows Nil All 
+instance Flows Register All 
+instance Flows Ads All 
+
+type family Join (l1 :: Purpose) (l2 :: Purpose) :: Purpose where
+    Join Register Ads = All
+    Join Ads Register = All
