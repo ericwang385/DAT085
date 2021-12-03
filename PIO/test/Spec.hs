36c73c617921531c3106b7bedcdaacab19662d4f
ericwang385 <eric.w385@hotmail.com>
2021-12-03 14:23:39 +0100

    some examples

--- PIO/test/Spec.hs
+++ PIO/test/Spec.hs
@@ -3,9 +3,11 @@
 import Data.List
 import Data.Char
 import Data.Maybe
-import Labled ( Labled )
+import Labled ( Labled, unLabled, toNil )
 import PIO ( PIO, run, nilLabled)
 import Purpose ( (:~:), Conj, All, Nil )
+import Data.Hashable
+
 
 main :: IO ()
 main = test
@@ -13,31 +15,40 @@ main = test
 data Register
 data Ads
 
+type DB = [(Labled * String, Labled * String)]
+
 test :: IO ()
 test = do   
         putStr "Input user name:"
         name <- getLine 
         -- user name can only be used in register
-        let sname = return name :: PIO Register String
+        let sname = return name :: Labled Register String
+        let db = return [] :: Labled Nil DB
         -- hashed name can be used in marketing
         let hname = hashName sname
         putStr "Input user email"
         mail <- getLine
-        let smail = return mail :: PIO Ads String
-        r <- run $ regist sname smail
+        let smail = return mail :: Labled (Conj Register Ads) String
+        r <- run $ regist db sname smail
+        if unLabled r then putStrLn "Register Success!" else putStrLn "Register Failed"
         m <- run $ marketing hname smail
         return()
 
-regist :: (l1 :~: Register, l2 :~: Ads) => PIO l1 String -> PIO l2 String -> PIO Nil Bool
-regist name mail = do
-                let m = nilLabled mail
-                let n = nilLabled name
-                n' <- n
-                m' <- m
-                return $ (length n' > 8) && isJust(find isDigit m')
+regist :: (l1 :~: Register, l2 :~: Register) => Labled Nil DB -> Labled l1 String -> Labled l2 String -> PIO Nil Bool
+regist db name mail = do
+                let m = unLabled mail
+                let n = unLabled name
+                res <- userExist db name
+                if res then return False else return True
 
-marketing :: (l1 :~: Ads, l2 :~: Ads) => PIO l1 String -> PIO l2 String -> PIO Nil Bool
-marketing = undefined
+userExist :: (l1 :~: Register) => Labled Nil DB -> Labled l1 String -> PIO Nil Bool
+userExist db name  = return True
 
-hashName :: (l1 :~: Register) => PIO l1 String -> PIO Nil String
-hashName = undefined
+marketing :: (l1 :~: Ads, l2 :~: Ads) => Labled l1 String -> Labled l2 String -> PIO Nil Bool
+marketing name string = return True
+
+hashName :: (l1 :~: Register) => Labled l1 String -> Labled Nil String
+hashName name = do
+                let n = unLabled name
+                return (show $ hash n) :: Labled Nil String
+                
