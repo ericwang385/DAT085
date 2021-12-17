1c6f3761407befe7c86812df39b9dff5e19683ac
ericwang385 <eric.w385@hotmail.com>
2021-12-17 16:36:14 +0100

    adjust test case

--- PIO/src/PIO.hs
+++ PIO/src/PIO.hs
@@ -1,26 +1,26 @@
 module PIO where
 
-import Labeled (Labeled, extract)
-import Control.Monad (ap)
-
-newtype PIO l a = MkPIO (IO (Labeled l a))
-
--- -- Shall only be Monad not Functor for the definition of purpose limitation
-instance Monad (PIO l) where
-    return a = MkPIO $ return $ return a
-
-    MkPIO m >>= k = MkPIO $ do
-            pa <- m
-            let MkPIO m' = k (extract pa)
-            m'
-
--- Appeals simply to meet Haskell demand
-instance Applicative (PIO l) where
-    pure  = return
-    (<*>) = ap
-
-instance Functor (PIO l) where
-    fmap f (MkPIO a) = MkPIO $ fmap f <$> a
+--import Labeled (Labeled, extract)
+--import Control.Monad (ap)
+--
+--newtype PIO l a = MkPIO (IO (Labeled l a))
+--
+---- -- Shall only be Monad not Functor for the definition of purpose limitation
+--instance Monad (PIO l) where
+--    return a = MkPIO $ return $ return a
+--
+--    MkPIO m >>= k = MkPIO $ do
+--            pa <- m
+--            let MkPIO m' = k (extract pa)
+--            m'
+--
+---- Appeals simply to meet Haskell demand
+--instance Applicative (PIO l) where
+--    pure  = return
+--    (<*>) = ap
+--
+--instance Functor (PIO l) where
+--    fmap f (MkPIO a) = MkPIO $ fmap f <$> a
 
 -- toPIO :: IO a -> PIO l a
 -- toPIO io = MkPIO $ io <&> return
--- PIO/test/Spec.hs
+++ PIO/test/Spec.hs
@@ -1,19 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 
 import Test.Hspec
-import Data.List
-import Data.Char
-import Data.Maybe
-import Data.Hashable
-import Labeled (Labeled, up, join, dup, com, extract, unLabeled, down)
+
+import Lib
+import Labeled (Labeled, up, join, dup, extract)
 import Purpose (Purpose( Register, Ads, All, Nil ))
 
 username = return "testName" :: Labeled Register String
 username' = return "testName2" :: Labeled Register String
 usermail = return "testMain" :: Labeled Ads String
 usermail' = return "testMain2" :: Labeled Ads String
-type DB = Labeled All [(Labeled Register String, Labeled Ads String)]
 db = return [(username, usermail)] :: DB
+
 
 main :: IO ()
 main = hspec spec
@@ -29,43 +27,16 @@ spec = do
       join (return $ return "testName" :: Labeled Register (Labeled Ads String)) `shouldBe` (return "testName" :: Labeled Nil String)
   describe "Registration example" $ do
     it "Test existUser" $ do
-      existUser db username `shouldBe` (return True :: Labeled All Bool)
+      existUser db username `shouldBe` (return True :: Labeled Register Bool)
     it "Test updateDB" $ do
       let res = existUser db username'
-      if extract res then res `shouldBe` (return True :: Labeled All Bool)
+      if extract res then res `shouldBe` (return True :: Labeled Register Bool)
       else do
         let newDB = updateDB db username' usermail'
         newDB `shouldBe` (return [(username, usermail), (username', usermail')] :: DB)
     it "Test Regist" $ do
-      register db username usermail `shouldBe` (return True :: Labeled All Bool)
+      register db username usermail `shouldBe` (return True :: Labeled Nil Bool)
 
 
-existUser :: DB -> Labeled Register String -> Labeled All Bool
-existUser db name = do
-              let db' = unLabeled db
-              let nameList = map fst db'
-              return $ isJust $ find (==name) nameList
 
-updateDB :: DB -> Labeled Register String -> Labeled Ads String -> DB
-updateDB db name mail = do
-              let db' = unLabeled db
-              return $ db' ++ [(name, mail)]
 
-sendMail :: Labeled Ads String -> Labeled Ads String -> Labeled All Bool
-sendMail hname mail = do
-            let mail' = extract mail
-            if length mail' > 8 then return True
-            else return False
-
-hashName :: Labeled Register String -> Labeled Nil String
-hashName = return . show . hash . extract
-
-register :: DB -> Labeled Register String -> Labeled Ads String -> Labeled All Bool
-register db name mail = do
-            let res = existUser db name
-            if (not . unLabeled) res
-              then updateDB db name mail
-            else undefined
-            let hname = hashName name
-            sendMail (down hname) mail
-            return True
