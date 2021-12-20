dbbc5b759329ace04b759e3d4dce2a7558078105
ericwang385 <eric.w385@hotmail.com>
2021-12-20 10:35:37 +0100

    new PIO test case

---
+++ PIO/test/Register.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DataKinds #-}
+
+module Register where
+
+import Lib
+import Ref
+import PIO
+import Labeled
+import Data.IORef
+import Purpose(Purpose(Register, Ads, All, Nil), Join)
+
+testname = return "testName" :: Labeled Register String
+testname' = return "testName2" :: Labeled Register String
+testmail = return "testMail" :: Labeled Ads String
+testmail' = return "testMail2" :: Labeled Ads String
+
+
+main :: IO ()
+main = do
+    putStrLn "Example program for PIO"
+    testdb <- newIORef [(testname, testmail)]
+    putStrLn "Enter username"
+    username <- getLine
+    putStrLn "Enter usermail"
+    usermail <- getLine
+    let db = createRef testdb :: Ref All DB
+    let username' = return username :: Labeled Register String
+    let usermail' = return usermail :: Labeled Ads String
+    res_exist <- run $ existUser db username'
+    if extract res_exist then putStrLn "User already exist"
+    else do
+      run $ updateDB db username' usermail'
+      putStrLn "Register success"
+      return ()
