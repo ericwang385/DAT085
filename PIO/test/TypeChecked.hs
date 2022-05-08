99d37ffe94693b3dc8e606cbd4ce0be0aea39cc2
ericwang385 <eric.w385@hotmail.com>
2022-05-08 15:35:15 +0200

    Modify Examples

--- PIO/test/TypeChecked.hs
+++ PIO/test/TypeChecked.hs
@@ -6,24 +6,31 @@ module TypeChecked where
 
 import Labeled
 import Prelude hiding (Monad(..))
-import Effect
+import Control.Effect
 import Data.Type.Set
-import Data.Kind (Type)
+import Purpose (Natural)
 
-data SendMail
-data ReadDB
-data WriteDB
-data Verify
+type SendMail = '[Natural 1]
+type ReadDB = '[Natural 2]
+type WriteDB = '[Natural 3]
+type Verify = '[Natural 4]
 
 
-type Login = Set '[ReadDB, Verify]
-type Marketing = Set '[ReadDB, SendMail]
-type Register = Set '[ReadDB, WriteDB, SendMail, Verify]
+type Login = Union ReadDB Verify
+type Marketing = Union ReadDB SendMail
+type Register = Union ReadDB (Union WriteDB (Union SendMail Verify))
 
-username = tag "TestName1" :: Labeled Register String
-usermail = tag "Testmail1" :: Labeled Login String
-userIP = tag "TestIP" :: Labeled Verify String
-password = tag "TestPassword" :: Labeled Login String
+username :: Labeled (Set Register) String
+username = tag "TestName1"
+
+usermail :: Labeled (Set Login) String
+usermail = tag "Testmail1"
+
+userIP :: Labeled (Set Verify) String
+userIP = tag "TestIP"
+
+password :: Labeled (Set Login) String
+password = tag "TestPassword"
 
 register :: Labeled (Set '[]) Bool
 register = verifyIP userIP >>=
@@ -33,31 +40,31 @@ register = verifyIP userIP >>=
                    else updateDB username usermail password >>=
            \ans3 -> if not ans3 then tag False
                    else sendmail usermail >>=
-           \ans4 -> if ans4 then tag True else tag False
+           \ans4 -> if ans4 then tag True :: Labeled (Set '[]) Bool else (up . return) False
 
 
 login :: Labeled (Set '[]) Bool
 login = verifyIP userIP >>=
         \ans1 -> if not ans1 then tag False
                  else checkPass username password >>=
-        \ans2 -> if ans2 then tag True else tag False
+        \ans2 -> if ans2 then tag True :: Labeled (Set '[]) Bool  else tag False :: Labeled (Set '[]) Bool
 
-checkPass :: Labeled Register String -> Labeled Login String -> Labeled Verify Bool
-checkPass name pass = password >>= \p -> pass >>= \p' -> if p == p' then tag True else tag False
+checkPass :: Labeled (Set Register) String -> Labeled (Set Login) String -> Labeled (Set Verify) Bool
+checkPass _ pass = password >>= \p -> pass >>= \p' -> if p == p' then tag True :: Labeled (Set Verify) Bool else tag False :: Labeled (Set Verify) Bool
 
-sendmail :: Labeled p String -> Labeled SendMail Bool
+sendmail :: Labeled p String -> Labeled (Set SendMail) Bool
 sendmail _ = tag True
 
-userExist :: Labeled p String -> Labeled Verify Bool
-userExist name = searchDB name >>= \ans -> if ans then tag True else tag False
+userExist :: Labeled p String -> Labeled (Set Verify) Bool
+userExist name = searchDB name >>= \ans -> if ans then tag True :: Labeled (Set Verify) Bool else tag False :: Labeled (Set Verify) Bool
 
-verifyIP :: Labeled p String -> Labeled Verify Bool
+verifyIP :: Labeled p String -> Labeled (Set Verify) Bool
 verifyIP _ = tag True
 
-searchDB :: Labeled p String -> Labeled Login Bool
+searchDB :: Labeled p String -> Labeled (Set Login) Bool
 searchDB _ = tag True
 
-updateDB :: Labeled p1 String -> Labeled p2 String -> Labeled p3 String -> Labeled WriteDB Bool
+updateDB :: Labeled p1 String -> Labeled p2 String -> Labeled p3 String -> Labeled (Set WriteDB) Bool
 updateDB _ _ _ = tag True
 
 
