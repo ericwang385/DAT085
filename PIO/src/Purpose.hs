51bf5f76e8f6f657742fafb45135fc3d97521d86
ericwang385 <eric.w385@hotmail.com>
2022-05-08 15:34:27 +0200

    Change Join to Set Intersection

--- PIO/src/Purpose.hs
+++ PIO/src/Purpose.hs
@@ -10,31 +10,30 @@ module Purpose where
 
 import Data.Kind (Constraint)
 import Data.Type.Set
-import GHC.TypeLits
+import GHC.TypeLits ( Nat, CmpNat, type (+) )
 import Prelude hiding (Monad(..))
 
-data All
+type All = Set '[Natural 0]
 
---type instance Cmp a a = 'EQ
-type instance Cmp a b = CmpSymbol a b
+data Natural (a :: Nat) where
+    Z :: Natural 0
+    S :: Natural n -> Natural (n + 1)
+
+type instance Cmp (Natural n) (Natural m) = CmpNat n m
+--type instance Cmp a b = CmpSymbol a b
+
+type Intersection s t = Dup (Sort (s :++ t))
+
+type family Dup t where
+    Dup '[]           = '[]
+    Dup '[e]          = '[]
+    Dup (e ': e ': s) = e ': Dup s
+    Dup (e ': f ': s) = Dup (f ': s)
 
 type family Join a b where
-  Join (Set a) (Set b) = Set(Union a b)
-  Join a (Set b) = Set(Union '[a] b)
+  Join All (Set b) = Set b
+  Join (Set a) (Set b) = Set (Intersection a b)
 
 type family CanFlowTo a b :: Constraint where
   CanFlowTo All a = a ~ a
-  CanFlowTo (Set (a ': b)) (Set (a ': c)) = CanFlowTo (Set b) (Set c)
-  CanFlowTo (Set (a ': b)) (Set (c ': d)) = CanFlowTo (Set b) (Set (c ': d))
-  CanFlowTo (Set (a ': b)) a = a ~ a
-  CanFlowTo (Set (a ': b)) c = CanFlowTo (Set b) c
-  CanFlowTo a (Set '[]) = a ~ a
-  --CanFlowTo a (Set (a ': c)) = a ~ a
-  --CanFlowTo a (Set (b ': c)) = CanFlowTo a (Set c)
-
---  CanFlowTo (Set (a1 ': a2)) (Set (a1 ': b)) = CanFlowTo a2 b
---  CanFlowTo (Purpose a1 a2) (Purpose a2 b) = CanFlowTo a1 b
---  CanFlowTo a1 (Purpose a1 a2) = a1 ~ a1
---  CanFlowTo a2 (Purpose a1 a2) = a1 ~ a1
---  CanFlowTo (Purpose a b) c = (CanFlowTo a c, CanFlowTo b c)
---  CanFlowTo a b = a ~ b
+  CanFlowTo (Set a) (Set b) = Union a b ~ a
