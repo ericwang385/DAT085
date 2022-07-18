06667268a4961bb3fcd6cc553ed698f2015d2c72
ericwang385 <eric.w385@hotmail.com>
2022-07-18 12:17:38 +0200

    Update using do notation

--- PIO/test/TypeChecked.hs
+++ PIO/test/TypeChecked.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RebindableSyntax #-}
 
 module TypeChecked where
 
 import Labeled
 import Prelude hiding (Monad(..))
 import Control.Effect
+
 import Data.Type.Set
 import Purpose (Natural, All, PUnion)
+import Control.Effect.Parameterised.ExtensibleState (ifThenElse)
 
 type SendMail = Set '[Natural 1]
 type ReadDB = Set '[Natural 2]
@@ -37,25 +40,37 @@ database :: DB
 database = tag [(username, password)]
 
 register :: Labeled (Set '[]) Bool
-register = userExist username >>=
-           \ans2 -> if ans2 then tag False
-                   else updateDB database username password >> sendmail usermail >>=
-           \ans4 -> if ans4 then tag True :: Labeled (Set '[]) Bool else (up . return) False
+register = do
+            ans <- userExist username
+            if ans then tag False :: Labeled (Set '[]) Bool
+            else do
+                _ <- updateDB database username password
+                ans3 <- sendmail usermail
+                if ans3 then tag True :: Labeled (Set '[]) Bool else tag False :: Labeled (Set '[]) Bool
 
 login :: Labeled (Set '[]) Bool
-login = verifyIP userIP >>=
-        \ans1 -> if not ans1 then tag False
-                 else checkPass username password >>=
-        \ans2 -> if ans2 then tag True :: Labeled (Set '[]) Bool  else tag False :: Labeled (Set '[]) Bool
+login = do
+         ans1 <- verifyIP userIP
+         if not ans1 then tag False
+         else do
+            ans2 <- checkPass username password
+            if ans2 then tag True :: Labeled (Set '[]) Bool  else tag False :: Labeled (Set '[]) Bool
 
 checkPass :: Labeled Register String -> Labeled Login String -> Labeled Verify Bool
-checkPass _ pass = password >>= \p -> pass >>= \p' -> if p == p' then tag True :: Labeled Verify Bool else tag False :: Labeled Verify Bool
+checkPass _ pass = do
+                p <- password
+                p' <- pass
+                if p == p' then tag True :: Labeled Verify Bool else tag False :: Labeled Verify Bool
 
 sendmail :: Labeled Login String -> Labeled SendMail Bool
-sendmail ma = ma >>= \a -> tag True :: Labeled SendMail Bool
+sendmail ma = do
+            _ <- ma
+            tag True :: Labeled SendMail Bool
 
 userExist :: Labeled Register String -> Labeled Verify Bool
-userExist name = searchDB name >>= \ans -> if ans then tag True :: Labeled Verify Bool else tag False :: Labeled Verify Bool
+userExist name = do
+                ans <- searchDB name
+                if ans then tag True :: Labeled Verify Bool else tag False :: Labeled Verify Bool
 
 verifyIP :: Labeled Verify String -> Labeled Verify Bool
 verifyIP ma = ma >>= \a -> tag True :: Labeled Verify Bool
@@ -64,6 +79,8 @@ searchDB :: Labeled Register String -> Labeled Login Bool
 searchDB _ = tag True :: Labeled Login Bool
 
 updateDB :: DB -> Labeled Register String -> Labeled Login String -> DB
-updateDB db name pass = db >>= \rawdb -> tag (rawdb ++ [(name, pass)]) :: DB
+updateDB db name pass = do
+                    rawdb <- db
+                    tag (rawdb ++ [(name, pass)]) :: DB
 
 
